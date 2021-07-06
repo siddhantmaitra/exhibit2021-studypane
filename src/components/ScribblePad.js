c232842b30634e4fa8688e3414c0a445ffacc69d
siddhantmaitra <sddhnt.mtra@gmail.com>
2021-07-06 12:01:32 +0530

    styling v1.0

--- src/components/ScribblePad.js
+++ src/components/ScribblePad.js
@@ -16,6 +16,7 @@ const ScribblePad = () => {
   return (
     <>
       <h6>Scribble Pad</h6>
+          {/* <p>The Bee Movie copypasta dates back to 2013 where users would post the entire script of the Bee Movie onto websites such as Reddit and Tumblr.[5] This was popularised around the time when edits of the film were first being posted and popularized on YouTube in late 2016.[6]</p> */}
       <textarea
         cols="30"
         rows="10"
